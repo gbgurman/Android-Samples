3fd96ac51f503b39d992a02492171f9707f03fc0
gbgurman <pinkhatapps@gmail.com>
2020-01-27 11:48:07 +0300

    Finished basic functionalities.

--- RetrofitExample/app/src/main/java/com/pinkhatapps/retrofitexample/MainActivity.java
+++ RetrofitExample/app/src/main/java/com/pinkhatapps/retrofitexample/MainActivity.java
@@ -1,14 +1,76 @@
 package com.pinkhatapps.retrofitexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.Headers;
+import retrofit.http.Query;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String API_URL = "https://api.ipify.org/";
+    TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        txtResult = findViewById(R.id.txtResult);
+
+        Button btn = findViewById(R.id.btnGetResults);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getResults();
+            }
+        });
+    }
+
+    private void getResults() {
+        RestAdapter adapter = new RestAdapter.Builder().setEndpoint(API_URL).build();
+        ApiInterface api = adapter.create(ApiInterface.class);
+        api.getIP(new Callback<ApiResponse>() {
+            @Override
+            public void success(ApiResponse apiResponse, Response response) {
+                Toast.makeText(MainActivity.this, "Response status: " + response.getStatus(), Toast.LENGTH_LONG).show();
+                txtResult.setText(String.format("Your IP is: %s", apiResponse.ip));
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                String errMsg = error == null ? "n/a" : error.getMessage();
+                Log.d("ERROR", errMsg);
+                Toast.makeText(MainActivity.this, String.format("No response. Error: %s", errMsg), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    interface ApiInterface {
+        @Headers({"Accept: application/json"})
+        @GET("/?format=json")
+        void getIP(Callback<ApiResponse> callback);
+    }
+    class ApiResponse {
+        public String ip;
     }
 }
+
